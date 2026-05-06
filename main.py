dd0522006b1b18e979338aaab72da72e9ae99705
AndrewTechTips <andreww.steel69@gmail.com>
2026-05-06 20:30:43 +0300

    add email functionality to send images on motion detection

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import glob
+
 import cv2
 import time
 from emailing import send_email
@@ -8,6 +10,8 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
 while True:
     status = 0
     check, frame = video.read()
@@ -24,8 +28,6 @@ while True:
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-    cv2.imshow("My video", dil_frame)
-
     contours, check = cv2.findContours(
         dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
@@ -37,6 +39,11 @@ while True:
 
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
